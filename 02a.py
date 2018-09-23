0b8bafefe10235ca10855df116c22e3f6fbc87a2
macht <macht@users.noreply.github.com>
2018-09-23 13:19:10 -0500

    Improve solution 02a

--- 02a.py
+++ 02a.py
@@ -2,14 +2,7 @@ total = 0
 
 with open('input/02_input.txt', 'r') as file:
     for line in file:
-        low = high = None
-        row = line.split()
-        for i in row:
-            if low is None:
-                low = high = int(i)
-            else:
-                low = min(low, int(i))
-                high = max(high, int(i))
-        total += high - low
+        row = [int(i) for i in line.split()]
+        total += max(row) - min(row)
 
 print(total)
