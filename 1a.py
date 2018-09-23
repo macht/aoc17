983b6a4b18f925245bcd98cbfc30730d73abcc62
macht <macht@users.noreply.github.com>
2018-09-23 09:03:22 -0500

    Initial commit

---
+++ 1a.py
@@ -0,0 +1,15 @@
+with open('1a_input.txt', 'r') as file:
+    data = file.read()  
+
+data = data.rstrip()    # strip newline char
+num_list = list(data)   
+sum = 0
+
+for i, v in enumerate(num_list, start=0):
+    if i+1 == len(num_list):
+        if int(v) == int(num_list[0]):
+            sum = sum + int(v)
+    elif int(v) == int(num_list[i+1]):
+        sum = sum + int(v)
+
+print(sum)
