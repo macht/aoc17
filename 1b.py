16b113aa31d77b866a2aaada85313509c30f483a
macht <macht@users.noreply.github.com>
2018-09-23 09:33:49 -0500

    Added solution to 1b

---
+++ 1b.py
@@ -0,0 +1,16 @@
+with open('1a_input.txt', 'r') as file:
+    data = file.read()  
+
+data = data.rstrip()    # strip newline char
+num_list = list(data)   
+sum = 0
+j = int(len(num_list) / 2)
+
+for i, v in enumerate(num_list, start=0):
+    if i+1 > j:
+        if int(v) == int(num_list[i%j]):
+            sum = sum + int(v)
+    elif int(v) == int(num_list[i+j]):
+        sum = sum + int(v)
+
+print(sum)
