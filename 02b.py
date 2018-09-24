d527b4bb97fc431db0f05b25b99b32d4439920ef
macht <macht@users.noreply.github.com>
2018-09-24 15:20:42 -0500

    Rewrite 02b

--- 02b.py
+++ 02b.py
@@ -1,10 +1,4 @@
-total = 0
-
 with open('input/02_input.txt', 'r') as file:
-    for line in file:
-        row = sorted([int(i) for i in line.split()], key=None, reverse=True)
-        for dividend in row:
-            mod = [divisor for divisor in row if dividend % divisor == 0 and dividend != divisor]
-            if mod:
-                total += dividend / mod.pop()
-print(total)
+    data = [[int(x) for x in row.split()] for row in file]
+
+print(sum(x//y for r in data for x in r for y in r if x != y and x % y == 0))
